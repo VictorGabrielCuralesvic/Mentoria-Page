feea91169e60a279fd1c89eb00797a284bfacdbe
VictorGabrielCuralesvic <miranhaomi@gmail.com>
2025-03-19 10:10:44 -0300

    Atualizado as rotas
    - Como no header ja foi feito anteriormente as rotas que vao ser seguidas pelo o usuario, foi atualizado as rotas web.php para as proximas paginas que vao ser feitos no blade.

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/courses', function () {
+    return view('courses');
+})->name('courses');
+
+Route::get('/trainers', function () {
+    return view('trainers');
+})->name('trainers');
+
+Route::get('/events', function() {
+    return view('events');
+})->name('events');
+
+Route::get('/pricing', function () {
+    return view('pricing');
+})->name('pricing');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
