ff8cfd44e9d0d2967f1049c75965e7b4e81375d9
VictorGabrielCuralesvic <miranhaomi@gmail.com>
2025-03-19 10:05:04 -0300

    Criado a secao de why-us
    - Foi criado a secao da why-us que contem as informacoes de pq escolher o produto do cliente
    - Tambem foi adicionado um pequena secao de contagem de usuarios do sistema

---
+++ resources/views/components/counts.blade.php
@@ -0,0 +1,30 @@
+<section id="counts" class="section counts light-background">
+    <div class="container" data-aos="fade-up" data-aos-delay="100">
+      <div class="row gy-4">
+        <div class="col-lg-3 col-md-6">
+          <div class="stats-item text-center w-100 h-100">
+            <span data-purecounter-start="0" data-purecounter-end="1232" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Estudantes</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="stats-item text-center w-100 h-100">
+            <span data-purecounter-start="0" data-purecounter-end="64" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Cursos</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="stats-item text-center w-100 h-100">
+            <span data-purecounter-start="0" data-purecounter-end="42" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Eventos</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="stats-item text-center w-100 h-100">
+            <span data-purecounter-start="0" data-purecounter-end="24" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Professores</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
