1425175d2ef9f64ca1e1d0154ed58d90a1ad1c22
VictorGabrielCuralesvic <miranhaomi@gmail.com>
2025-03-19 10:05:30 -0300

    Criado a secao de cursos
    - Criado a secao que contem os cursos que vao estar disponibilizados no sistema

---
+++ resources/views/components/courses.blade.php
@@ -0,0 +1,35 @@
+<section id="courses" class="courses section">
+    <div class="container section-title" data-aos="fade-up">
+      <h2>Cursos</h2>
+      <p>Cursos Populares</p>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+          <div class="course-item">
+            <img src="{{ asset('assets/img/course-1.jpg') }}" class="img-fluid" alt="...">
+            <div class="course-content">
+              <div class="d-flex justify-content-between align-items-center mb-3">
+                <p class="category">Web Development</p>
+                <p class="price">$70</p>
+              </div>
+              <h3><a href="course-details.html">Website Design</a></h3>
+              <p class="description">Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+              <div class="trainer d-flex justify-content-between align-items-center">
+                <div class="trainer-profile d-flex align-items-center">
+                  <img src="{{ asset('assets/img/trainers/trainer-1-2.jpg') }}" class="img-fluid" alt="">
+                  <a href="" class="trainer-link">Antonio</a>
+                </div>
+                <div class="trainer-rank d-flex align-items-center">
+                  <i class="bi bi-person user-icon"></i>&nbsp;50
+                  &nbsp;&nbsp;
+                  <i class="bi bi-heart heart-icon"></i>&nbsp;65
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Repetir para os outros cursos -->
+      </div>
+    </div>
+  </section>
