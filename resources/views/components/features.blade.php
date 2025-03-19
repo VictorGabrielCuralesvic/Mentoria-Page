ce92fbc99022ed725cf38ea0a778d724b33ade22
VictorGabrielCuralesvic <miranhaomi@gmail.com>
2025-03-19 10:07:31 -0300

    Criado uma simples feature
    - Foi criado uma feature simples que, ao clicar, retorna o usuario para o topo da landpage

---
+++ resources/views/components/features.blade.php
@@ -0,0 +1,13 @@
+<section id="features" class="features section">
+    <div class="container">
+      <div class="row gy-4">
+        <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="100">
+          <div class="features-item">
+            <i class="bi bi-eye" style="color: #ffbb2c;"></i>
+            <h3><a href="" class="stretched-link">Lorem Ipsum</a></h3>
+          </div>
+        </div>
+        <!-- Repetir para os outros itens de features -->
+      </div>
+    </div>
+  </section>
