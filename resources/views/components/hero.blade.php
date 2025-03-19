65b69ed13a68d12896fea1873dd6106b0d410bb9
VictorGabrielCuralesvic <miranhaomi@gmail.com>
2025-03-19 20:58:37 -0300

    Criado a Pagina de Cursos
    - Foi criado a pagina responsavel por ter as informacoes especificas dos cursos do website, o qual utiliza do layout ja existente anteriormente, mas adicionando secoes especificas da pagina

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,8 +1,8 @@
 <section id="hero" class="hero section dark-background">
     <img src="{{ asset('assets/img/hero-bg.jpg') }}" alt="" data-aos="fade-in">
     <div class="container">
-      <h2 data-aos="fade-up" data-aos-delay="100">Learning Today,<br>Leading Tomorrow</h2>
-      <p data-aos="fade-up" data-aos-delay="200">We are team of talented designers making websites with Bootstrap</p>
+      <h2 data-aos="fade-up" data-aos-delay="100">Aprendendo hoje,<br>Liderando o amanha</h2>
+      <p data-aos="fade-up" data-aos-delay="200">Somos um time apaixonado em desenvolver talentos</p>
       <div class="d-flex mt-4" data-aos="fade-up" data-aos-delay="300">
         <a href="{{ url('/courses') }}" class="btn-get-started">Get Started</a>
       </div>
