83324125633c1cad1e3ea4f1779727e24f50963c
VictorGabrielCuralesvic <miranhaomi@gmail.com>
2025-03-20 19:29:49 -0300

    Criado a pagina trainers
    - A pagina trainers (professores) foi criada utilizando o layout ja existente do sistema e adicionado o seu page title

---
+++ resources/views/pages/trainers.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('title', 'Professores - Mentoria')
+
+@section('content')
+<div class="page-title" data-aos="fade">
+    <div class="heading">
+        <div class="container">
+            <div class="row d-flex justify-content-center text-center">
+                <div class="col-lg-8">
+                    <h1>Professores</h1>
+                    <p class="mb-0">Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav class="breadcrumbs">
+        <div class="container">
+            <ol>
+                <li><a href="{{ route('home')}}">Home</a></li>
+                <li class="current">Professores</li>
+            </ol>
+        </div>
+    </nav>
+</div> {{-- End Page Title --}}
+
+@endsection
