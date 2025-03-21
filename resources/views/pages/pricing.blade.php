f2b758715df1f6773435543ac47864a38685fc59
VictorGabrielCuralesvic <miranhaomi@gmail.com>
2025-03-21 19:06:17 -0300

    Atualizado o pagina de preco
    - Foi adicionado a parte especifica da pagina de preco que é os itens de preços separados cada um em sua propria div, sem necessidade de usar um card

--- resources/views/pages/pricing.blade.php
+++ resources/views/pages/pricing.blade.php
@@ -24,3 +24,82 @@
         </div>
     </nav>
 </div> {{-- Page Title --}}
+
+{{-- Pricing Section --}}
+<section id="pricing" class="pricing section">
+    <div class="container">
+        <div class="row gy-3">
+            <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                <div class="pricing-item">
+                    <h3>Free</h3>
+                    <h4><sup>$</sup>0<span> / mes</span></h4>
+                    <ul>
+                        <li>Aida dere</li>
+                        <li>Nec feugiat nisl</li>
+                        <li>Nulla at volutpat dola</li>
+                        <li class="na">Pharetra massa</li>
+                        <li class="na">Massa ultricies mi</li>
+                    </ul>
+                    <div class="btn-wrap">
+                        <a href="#" class="btn-buy">Buy Now</a>
+                    </div>
+                </div>
+            </div> {{-- End Pricing Item --}}
+
+            <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                <div class="pricing-item">
+                    <h3>Premium</h3>
+                    <h4><sup>$</sup>19<span> / mes</span></h4>
+                    <ul>
+                        <li>Aida dere</li>
+                        <li>Nec feugiat nisl</li>
+                        <li>Nulla at volutpat dola</li>
+                        <li class="na">Pharetra massa</li>
+                        <li class="na">Massa ultricies mi</li>
+                    </ul>
+                    <div class="btn-wrap">
+                        <a href="#" class="btn-buy">Buy Now</a>
+                    </div>
+                </div>
+            </div> {{-- End Pricing Item --}}
+
+            <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                <div class="pricing-item">
+                    <h3>Deluxe</h3>
+                    <h4><sup>$</sup>29<span> / mes</span></h4>
+                    <ul>
+                        <li>Aida dere</li>
+                        <li>Nec feugiat nisl</li>
+                        <li>Nulla at volutpat dola</li>
+                        <li class="na">Pharetra massa</li>
+                        <li class="na">Massa ultricies mi</li>
+                    </ul>
+                    <div class="btn-wrap">
+                        <a href="#" class="btn-buy">Buy Now</a>
+                    </div>
+                </div>
+            </div> {{-- End Pricing Item --}}
+
+            <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                <div class="pricing-item">
+                    <h3>Ultimate</h3>
+                    <h4><sup>$</sup>39<span> / mes</span></h4>
+                    <ul>
+                        <li>Aida dere</li>
+                        <li>Nec feugiat nisl</li>
+                        <li>Nulla at volutpat dola</li>
+                        <li class="na">Pharetra massa</li>
+                        <li class="na">Massa ultricies mi</li>
+                    </ul>
+                    <div class="btn-wrap">
+                        <a href="#" class="btn-buy">Buy Now</a>
+                    </div>
+                </div>
+            </div> {{-- End Pricing Item --}}
+
+        </div>
+    </div>
+
+</section> {{-- /Pricing Section --}}
+    
+@endsection
